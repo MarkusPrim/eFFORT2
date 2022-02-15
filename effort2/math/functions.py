f3efa3f5cca839ece4384bd721d635fea9f93dfe
Markus Prim <markus.prim@desy.de>
2022-02-15 12:16:40 +0100

    Fix return value of quad integration routine to match scipy

--- effort2/math/functions.py
+++ effort2/math/functions.py
@@ -3,7 +3,7 @@ from uncertainties import unumpy as unp
 
 
 def diLog(z):
-    return -1 * quad(lambda t: unp.log(1-t)/t, 0, z)
+    return -1 * quad(lambda t: unp.log(1-t)/t, 0, z)[0]
 
 def spence(z):
     return -diLog(1 - z)
--- effort2/math/integrate.py
+++ effort2/math/integrate.py
@@ -1,6 +1,8 @@
-from scipy.special import roots_legendre, eval_legendre
+from scipy.special import roots_legendre
+import numpy as np
 
 
-def quad(f, a, b, order=19):
+def quad(f, a, b, order=19, **kwargs):
+    """Gaussiand Quadrature with Legendre polynomials. Same signature and return value as scipy.integrate.quad."""
     roots, weights = roots_legendre(order)
-    return (b - a) / 2 * sum(w_i * f((b - a) / 2 * x_i + (b + a) / 2) for w_i, x_i in zip(weights, roots))
+    return (b - a) / 2 * sum(w_i * f((b - a) / 2 * x_i + (b + a) / 2) for w_i, x_i in zip(weights, roots)), np.nan
