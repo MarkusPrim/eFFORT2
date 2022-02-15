02d33c0b86d7167e0c0ac559dfa96cca1fd524c8
Markus Prim <markus.prim@desy.de>
2022-02-15 19:56:56 +0100

    cache legendre roots

--- effort2/math/integrate.py
+++ effort2/math/integrate.py
@@ -1,8 +1,10 @@
+import functools
 from scipy.special import roots_legendre
 import numpy as np
 
 
 def quad(f, a, b, order=19, **kwargs):
     """Gaussiand Quadrature with Legendre polynomials. Same signature and return value as scipy.integrate.quad."""
-    roots, weights = roots_legendre(order)
+    cached_roots_legendre = functools.lru_cache(roots_legendre)
+    roots, weights = cached_roots_legendre(order)
     return (b - a) / 2 * sum(w_i * f((b - a) / 2 * x_i + (b + a) / 2) for w_i, x_i in zip(weights, roots)), np.nan
