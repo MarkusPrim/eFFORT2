3e671ab31b81917a200e92ddfe8685561888645c
Tommy <tommy.martinov@desy.de>
2023-11-20 19:20:58 +0100

    Add BLR form factors + alphaS corrections

---
+++ effort2/formfactors/BLR.py
@@ -0,0 +1,563 @@
+from ast import Lambda
+import numpy as np
+from effort2.formfactors.DStStAlphaSCorrections import DStStAlphaSCorrections
+
+
+class BToDStarStarBroad(DStStAlphaSCorrections):
+    """
+    TO DO: add description
+    """
+
+    def __init__(
+        self,
+        m_c: float,
+        m_b: float,
+        params: tuple,
+        # m_B: float,
+        # m_V: float,
+        # m_L: float=0,
+        alphaS: float=0,
+        LambdaBar: float=0.40,
+        LambdaBarPrime: float=0.80,
+        LambdaBarStar: float=0.76,
+        chi1: float=0,
+        chi2: float=0,
+    ):
+        super().__init__(m_c, m_b)
+        self.m_c = m_c
+        self.m_b = m_b
+        self.alphaS = alphaS / np.pi
+        self.LambdaBar = LambdaBar
+        self.LambdaBarPrime = LambdaBarPrime
+        self.LambdaBarStar = LambdaBarStar
+        self.chi1 = chi1
+        self.chi2 = chi2
+
+        self.Z1, self.zetap, self.zeta1 = self.set_model_parameters(params)
+        # TO DO: check parameter values from Hammer
+        # Scale mu = sqrt(mc*mb)
+        # Can ask Markus/Florian for details about BLR
+        # Certain values depend on renorm scheme
+        # The chromomagnetic terms chi1/2 are neglected in Hammer and certain Approximations.
+        # I'm not sure whether they should be set to 0 with the most up-to-date fit of the 3 zeta parameters.
+
+    def set_model_parameters(
+        self,
+        params: tuple,
+    ):
+        """
+        Sets the input model parameters zeta(1), zeta' and zeta1 (cf. Table V in arxiv:1711.03110).
+        Expects the parameters in that order.
+
+        Args:
+            params ([tuple]): input parameters for the broad D** form factor.
+        """
+        Z1, zetap, zeta1 = params
+        return Z1, zetap, zeta1
+    
+    def Gb(
+        self,
+        w: float,
+    ):
+        """
+        Used in all B -> D** broad form factors
+        """
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        zeta1 = self.zeta1
+        return ((1 + 2 * w) * LambdaBarStar - (2 + w) * LambdaBar) / (w + 1) - 2 * (w - 1) * zeta1
+
+    # Form factors for D0*
+    def gP(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        chi1 = self.chi1
+        chi2 = self.chi2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        zeta1 = self.zeta1
+        CP = self.CP(w)
+        Gb = self.Gb(w)
+        return (w - 1) * (1 + alphaS * CP) + epsilonC * (3 * (w * LambdaBarStar - LambdaBar) - 2 * (w**2 - 1) * zeta1 + (w -1) * (6 * chi1 - 2 * (w + 1) * chi2) - epsilonB * (w + 1) * Gb)
+
+    def gplus(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        zeta1 = self.zeta1
+        CA2 = self.CA2(w)
+        CA3 = self.CA3(w)
+        Gb = self.Gb(w)
+        return 0.5 * alphaS * (w - 1) * (CA2 + CA3) - epsilonC * (3 * (w * LambdaBarStar - LambdaBar) / (w + 1) - 2 * (w - 1) * zeta1) - epsilonB * Gb
+
+    def gminus(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        epsilonC = 1 / (2 * self.m_c)
+        chi1 = self.chi1
+        chi2 = self.chi2
+        CA1 = self.CA1(w)
+        CA2 = self.CA2(w)
+        CA3 = self.CA3(w)
+        return 1 + alphaS * (CA1 + 0.5 * (w - 1) * (CA2 + CA3)) + epsilonC * (6 * chi1 - 2 * (w + 1) * chi2)
+
+    def gT(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        chi1 = self.chi1
+        chi2 = self.chi2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        zeta1 = self.zeta1
+        Gb = self.Gb(w)
+        CT1 = self.CT1(w)
+        return 1 + alphaS * CT1 + epsilonC * (3 * (w * LambdaBarStar - LambdaBar) / (w + 1) - 2 * (w - 1) * zeta1 + 6 * chi1 - 2 * (w + 1) * chi2) - epsilonB * Gb
+
+    # Form factors for D1*
+    def gS(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        chi1 = self.chi1
+        chi2 = self.chi2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        zeta1 = self.zeta1
+        Gb = self.Gb(w)
+        CS = self.CS(w)
+        return 1 + alphaS * CS - epsilonC * ((w * LambdaBarStar - LambdaBar) / (w + 1) - 2 * (w - 1) * zeta1 + 2 * chi1 - 2 * (w + 1) * chi2) - epsilonB * Gb
+    
+    def gV1(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        chi1 = self.chi1
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        Gb = self.Gb(w)
+        CV1 = self.CV1(w)
+        return (w - 1) * (1 + alphaS * CV1) + epsilonC * (w * LambdaBarStar - LambdaBar - 2 * (w - 1) * chi1) - epsilonB * (w + 1) * Gb
+    
+    def gV2(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        chi2 = self.chi2
+        epsilonC = 1 / (2 * self.m_c)
+        zeta1 = self.zeta1
+        CV2 = self.CV2(w)
+        return - alphaS * CV2 + 2 * epsilonC * (zeta1 - chi2)
+
+    def gV3(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        chi1 = self.chi1
+        chi2 = self.chi2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        zeta1 = self.zeta1
+        Gb = self.Gb(w)
+        CV1 = self.CV1(w)
+        CV3 = self.CV3(w)
+        return -1 - alphaS * (CV1 + CV3) - epsilonC * ((w * LambdaBarStar - LambdaBar) / (w + 1) + 2 * (zeta1 - chi1 + chi2)) + epsilonB * Gb
+    
+    def gA(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        chi1 = self.chi1
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        Gb = self.Gb(w)
+        CA1 = self.CA1(w)
+        return 1 + alphaS * CA1 + epsilonC * ((w * LambdaBarStar - LambdaBar) / (w + 1) - 2 * chi1) - epsilonB * Gb
+    
+    def gT1(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        chi1 = self.chi1
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        Gb = self.Gb(w)
+        CT1 = self.CT1(w)
+        CT2 = self.CT2(w)
+        return -1 - alphaS * (CT1 + (w - 1) * CT2) + epsilonC * ((w * LambdaBarStar - LambdaBar) / (w + 1) + 2 * chi1) + epsilonB * Gb
+    
+    def gT2(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        chi1 = self.chi1
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        Gb = self.Gb(w)
+        CT1 = self.CT1(w)
+        CT3 = self.CT3(w)
+        return 1 + alphaS * (CT1 - (w - 1) * CT3) + epsilonC * ((w * LambdaBarStar - LambdaBar) / (w + 1) - 2 * chi1) + epsilonB * Gb
+
+    def gT3(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        zeta1 = self.zeta1
+        chi2 = self.chi2
+        epsilonC = 1 / (2 * self.m_c)
+        CT2 = self.CT2(w)
+        return -alphaS * CT2 + 2 * epsilonC * (zeta1 + chi2)
+
+
+class BToDStarStarNarrow(DStStAlphaSCorrections):
+    """
+    TO DO: add description
+    """
+
+    def __init__(
+        self,
+        m_c: float,
+        m_b: float,
+        params: tuple,
+        alphaS: float=0,
+        LambdaBar: float=0.40,
+        LambdaBarPrime: float=0.80,
+        LambdaBarStar: float=0.76,
+        eta1: float=0,
+        eta2: float=0,
+        eta3: float=0,
+    ):
+        super().__init__(m_c, m_b)
+        self.m_c = m_c
+        self.m_b = m_b
+        self.alphaS = alphaS / np.pi
+        self.LambdaBar = LambdaBar
+        self.LambdaBarPrime = LambdaBarPrime
+        self.LambdaBarStar = LambdaBarStar
+        self.eta1 = eta1
+        self.eta2 = eta2
+        self.eta3 = eta3
+
+        self.T1, self.taup, self.tau1, self.tau2 = self.set_model_parameters(params)
+
+    def set_model_parameters(
+        self,
+        params: tuple,
+    ):
+        """
+        Sets the input model parameters tau(1), tau', tau1 and tau2 (cf. Table V in arxiv:1711.03110).
+        Expects the parameters in that order.
+
+        Args:
+            params ([tuple]): input parameters for the narrow D** form factor.
+        """
+        T1, taup, tau1, tau2 = params
+        return T1, taup, tau1, tau2
+
+    def Fp(
+        self,
+        w: float,
+    ):
+        LambdaBar = self.LambdaBar
+        LambdaBarStar = self.LambdaBarStar
+        tau1 = self.tau1
+        tau2 = self.tau2
+        return LambdaBar + LambdaBarStar - (2 * w + 1) * tau1 - tau2
+
+    # Form factors for D1
+    # The f form factors have to be multiplied by sqrt(6)
+    def fS(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarPrime = self.LambdaBarPrime
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta2 = self.eta2
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CS = self.CS(w)
+        return -2 * (w + 1) * (1 + alphaS * CS) - 2 * epsilonB * (w - 1) * Fb - epsilonC * (4 * (w * LambdaBarPrime - LambdaBar) - 2 * (w - 1) * ((2 * w + 1) * tau1 + tau2) + 2 * (w + 1) * (6 * eta1 + 2 * (w - 1) * eta2 - eta3))
+
+    def fV1(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarPrime = self.LambdaBarPrime
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CV1 = self.CV1(w)
+        return (1 - w**2) * (1 + alphaS * CV1) - epsilonB * (w**2 - 1) * Fb - epsilonC * (4 * (w + 1) * (w * LambdaBarPrime - LambdaBar) - (w**2 - 1) * (3 * tau1 - 3 * tau2 + 2 * eta1 + 3 * eta3))
+
+    def fV2(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta2 = self.eta2
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CV1 = self.CV1(w)
+        CV2 = self.CV2(w)
+        return -3 - alphaS * (3 * CV1 + 2 * (w + 1) * CV2) - 3 * epsilonB * Fb - epsilonC * ((4 * w - 1) * tau1 + 5 * tau2 + 10 * eta1 + 4 * (w - 1) * eta2 - 5 * eta3)
+
+    def fV3(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarPrime = self.LambdaBarPrime
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta2 = self.eta2
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CV1 = self.CV1(w)
+        CV3 = self.CV3(w)
+        return w - 2 - alphaS * ((2 - w) * CV1 + 2 * (w + 1) * CV3) - epsilonB * (w + 2) * Fb + epsilonC * (4 * (w * LambdaBarPrime - LambdaBar) + (w + 2)* tau1 + (2 + 3 * w) * tau2 - 2 (w + 6) * eta1 - 4 * (w - 1) * eta2 - (3 * w - 2) * eta3)
+
+    def fA(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarPrime = self.LambdaBarPrime
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta2 = self.eta2
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CA1 = self.CA1(w)
+        return -(w + 1) * (1 + alphaS * CA1) - epsilonB * (w - 1) * Fb - epsilonC * (4 * (w * LambdaBarPrime - LambdaBar) - 3 * (w - 1) * (tau1 - tau2) -  (w + 1) * (2 * eta1 + 3 * eta3))
+
+    def fT1(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarPrime = self.LambdaBarPrime
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CT1 = self.CT1(w)
+        CT2 = self.CT2(w)
+        return (w + 1) * (1 + alphaS * (CT1 + (w - 1) * CT2)) + epsilonB * (w - 1) * Fb - epsilonC * (4 * (w * LambdaBarPrime - LambdaBar) - 3 * (w - 1) * (tau1 - tau2) + (w + 1) * (2 * eta1 + 3 * eta3))
+
+    def fT2(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        LambdaBar = self.LambdaBar
+        LambdaBarPrime = self.LambdaBarPrime
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta2 = self.eta2
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CT1 = self.CT1(w)
+        CT3 = self.CT3(w)
+        return -(w + 1) * (1 + alphaS * (CT1 - (w - 1) * CT3)) + epsilonB * (w - 1) * Fb - epsilonC * (4 * (w * LambdaBarPrime - LambdaBar) - 3 * (w - 1) * (tau1 - tau2) - (w + 1) * (2 * eta1 + 3 * eta3))
+
+    def fT3(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta2 = self.eta2
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CT1 = self.CT1(w)
+        CT2 = self.CT2(w)
+        CT3 = self.CT3(w)
+        return 3 + alphaS * (3 * CT1 - (2 - w) * CT2 + 3 * CT3) + 3 * epsilonB * Fb - epsilonC * ((4 * w - 1) * tau1 + 5 * tau2 - 10 * eta1 - 4 * (w - 1) * eta2 + 5 * eta3)
+
+
+    # Form factors for D2*
+    def kP(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta2 = self.eta2
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CP = self.CP(w)
+        return 1 + alphaS * CP + epsilonB * Fb + epsilonC * ((2 * w + 1) * tau1 + tau2 - 2 * eta1 - 2 * (w - 1) * eta2 + eta3)
+
+    def kV(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CV1 = self.CV1(w)
+        return -1 - alphaS * CV1 - epsilonB * Fb - epsilonC * (tau1 - tau2 - 2 * eta1 + eta3)
+
+    def kA1(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CA1 = self.CA1(w)
+        return -(w + 1) * (1 + alphaS * CA1) - epsilonB * (w - 1) * Fb - epsilonC * ((w - 1) * (tau1 - tau2) - (w + 1) * (2 * eta1 - eta3))
+
+    def kA2(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        tau1 = self.tau1
+        epsilonC = 1 / (2 * self.m_c)
+        eta2 = self.eta2
+        CA2 = self.CA2(w)
+        return alphaS * CA2 - 2 * epsilonC (tau1 + eta2)
+
+    def kA3(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        eta1 = self.eta1
+        eta2 = self.eta2
+        eta3 = self.eta3
+        Fb = self.Fb(w)
+        CA1 = self.CA1(w)
+        CA3 = self.CA3(w)
+        return 1 + alphaS * (CA1 + CA3) + epsilonB * Fb - epsilonC * (tau1 + tau2 + 2 * eta1 - 2 * eta2 - eta3)
+
+    def kT1(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        epsilonC = 1 / (2 * self.m_c)
+        eta1 = self.eta1
+        eta3 = self.eta3
+        CT1 = self.CT1(w)
+        CT2 = self.CT2(w)
+        CT3 = self.CT3(w)
+        return 1 + alphaS * (CT1 + 0.5 * (w - 1) * (CT2 - CT3)) - epsilonC * (2 * eta1 - eta3)
+
+    def kT2(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        tau1 = self.tau1
+        tau2 = self.tau2
+        epsilonC = 1 / (2 * self.m_c)
+        epsilonB = 1 / (2 * self.m_b)
+        Fb = self.Fb(w)
+        CT2 = self.CT2(w)
+        CT3 = self.CT3(w)
+        return 0.5 * alphaS * (w + 1) * (CT2 + CT3) + epsilonB * Fb - epsilonC * (tau1 - tau2)
+
+    def kT3(
+        self,
+        w: float,
+    ):
+        alphaS = self.alphaS
+        tau1 = self.tau1
+        epsilonC = 1 / (2 * self.m_c)
+        eta2 = self.eta2
+        CT2 = self.CT2(w)
+        return -alphaS * CT2 + 2 * epsilonC * (tau1 - eta2)
+
+
+if __name__ == "__main__":
+    pass
---
+++ effort2/formfactors/BLR_helper.py
@@ -0,0 +1,159 @@
+import numpy as np
+from effort2.math.functions import diLog
+
+
+class DStStAlphaSCorrections:
+    """
+    Class to compute the C functions used in the BLR model for B -> D** form factors.
+    Taken from Phys. Rev. D 95, 115008 Appendix A.
+    """
+
+    def __init__(
+        self,
+        m_c: float,
+        m_b: float,
+    ) -> None:
+        self.z = m_c / m_b
+        self.wz = 0.5 (self.z + 1 / self.z)
+
+
+    def r(
+        self,
+        w: float,
+    ) -> float:
+        wp = w + np.sqrt(w**2 - 1)
+        return np.log(wp) / np.sqrt(w**2 - 1)
+
+
+    def Omega(
+        self,
+        w: float,
+    ):
+        z = self.z
+        r = self.r(w)
+        wp = w + np.sqrt(w**2 - 1)
+        wm = w - np.sqrt(w**2 - 1)
+        return w / (2 * np.sqrt(w**2 - 1)) * (2 * diLog(1 - wm * z) - 2 * diLog(1 - wp * z) + diLog(1 - wp**2) - diLog(1 - wm**2)) - w * r * np.log(z) + 1
+
+
+    def CS(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        Omega = self.Omega(w)
+        return 1 / (3 * z * (w - wz)) * (2 * z * (w - wz) * Omega - (w - 1) * (z + 1) * (z + 1) * r + (z**2 -1) * np.log(z))
+
+
+    def CP(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        Omega = self.Omega(w)
+        return 1 / (3 * z * (w - wz)) * (2 * z * (w - wz) * Omega - (w + 1) * (z - 1) * (z - 1) * r + (z**2 -1) * np.log(z))
+    
+
+    def CV1(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        Omega = self.Omega(w)
+        return 1 / (6 * z * (w - wz)) * (2 * (w + 1) * ((3 * w - 1) * z - z**2 - 1) * r + (12 * z * (wz - w) - (z**2 -1) * np.log(z)) + 4 * z * (w - wz) * Omega)
+    
+    def CV2(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        return -1 / (6 * z**2 * (w - wz) * (w - wz)) * (((4 * w**2 + 2 * w) * z**2 - (2 * w**2 + 5 * w -1) * z - (w + 1) * z**3 + 2) * r + z * ( 2 * (z - 1) * (wz - w) + (z**2 - (4 * w - 2) * z + (3 - 2 * w)) * np.log(z)))
+
+
+    def CV3(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        return 1 / (6 * z * (w - wz) * (w - wz)) * (((2 * w**2 + 5 * w - 1) * z**2 - (4 * w**2 + 2 * w) * z - 2 * z**3 + w + 1) * r + (2 * z * (z - 1) * (wz - w) + ((3 - 2 * w) * z**2 + (2 - 4 * w) * z + 1) * np.log(z)))
+    
+
+    def CA1(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        Omega = self.Omega(w)
+        return 1 / (6 * z * (w - wz)) * (2 * (w - 1) * ((3 * w + 1) * z - z**2 - 1) * r + (12 * z * (wz - w) - (z**2 -1) * np.log(z)) + 4 * z * (w - wz) * Omega)
+
+
+    def CA2(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        return -1 / (6 * z**2 * (w - wz) * (w - wz)) * (((4 * w**2 - 2 * w) * z**2 - (2 * w**2 - 5 * w -1) * z - (w - 1) * z**3 + 2) * r + z * ( 2 * (z + 1) * (wz - w) + (z**2 - (4 * w + 2) * z + (3 + 2 * w)) * np.log(z)))
+    
+
+    def CA3(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        return 1 / (6 * z * (w - wz) * (w - wz)) * ((2 * z**3 + (2 * w**2 - 5 * w -1) * z**2 + (4 * w**2 - 2 * w) * z - w + 1) * r + (2 * z * (z + 1) * (wz - w) - ((2 * w + 3) * z**2 - (4 * w + 2) * z + 1) * np.log(z)))
+
+
+    def CT1(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        Omega = self.Omega(w)
+        return 1 / (3 * z * (w - wz)) * ((w - 1) * ((4 * w + 2) * z - z**2 - 1) * r + (6 * z * (wz - w) - (z**2 - 1) * np.log(z)) + 2 * z * (w - wz) * Omega)
+    
+
+    def CT2(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        return 2 / (3 * z * (w - wz)) * ((1 - w * z) * r + z * np.log(z))
+    
+
+    def CT3(
+        self,
+        w: float,
+    ):
+        z = self.z
+        wz = self.wz
+        r = self.r(w)
+        return 2 / (3 * (w - wz)) * ((w - z) * r + np.log(z))
+
+
+class IWFunctions:
+    """
+    Class to define the Isgure-Wise functions used in the BLR model for B -> D** form factors
+    """
+
+    def __init__(self) -> None:
+        pass
